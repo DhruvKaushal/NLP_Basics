3a496afbc68a1c3dcbe3d0bbac34542d525525db
DhruvKaushal <dhruvkaushal1997@gmail.com>
2019-01-22 16:13:44 +0530

    Stemming and lemmatization in NLP

--- basic.py
+++ basic.py
@@ -1,6 +1,14 @@
 import nltk
-from nltk.book import *
+from nltk import PorterStemmer
+para = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 
-print(text7)
-text3.concordance("money")
-text3.count("money")
+#Stemming
+sentence = nltk.sent_tokenize(para)
+stemmer = PorterStemmer()
+
+for i in range(len(sentence)):
+    words = nltk.word_tokenize(sentence[i])
+    newwords = [stemmer.stem(word) for word in words]
+    sentence[i] = ' '.join(newwords)
+    
+#lemmatization
--- course1.py
+++ course1.py
@@ -3,3 +3,8 @@
 import re
 sent = "Hello there how are you"
 re.match(r"Hell", sent)
+
+#Substituting
+sent = "I love avengers"
+print(re.sub("avengers", "Justice league", sent))
+print(sent)
