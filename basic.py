5072df1c2c0ea8d9ef4717902819bfa845663909
DhruvKaushal <dhruvkaushal1997@gmail.com>
2019-01-22 16:20:25 +0530

    removal of stopwords

--- basic.py
+++ basic.py
@@ -12,3 +12,20 @@ for i in range(len(sentence)):
     sentence[i] = ' '.join(newwords)
     
 #lemmatization
+from nltk.stem import WordNetLemmatizer
+sentenceL = nltk.sent_tokenize(para)
+lemmatizer = WordNetLemmatizer()
+for i in range(len(sentenceL)):
+    wordsL = nltk.word_tokenize(sentenceL[i])
+    newwordsL = [lemmatizer.lemmatize(word) for word in wordsL]
+    sentenceL[i] = ' '.join(newwordsL)
+
+
+#Removal of stopwords
+from nltk.corpus import stopwords
+sentence = nltk.sent_tokenize(para)
+for i in range(len(sentence)):
+    words = nltk.word_tokenize(sentence[i])
+    newwords = [word for word in words if word not in stopwords.words('english')]
+    sentence[i] = ' '.join(newwords)
+    
