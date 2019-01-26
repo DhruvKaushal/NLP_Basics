39670134463f7a3e373dccd5e20e7a02632e0524
DhruvKaushal <dhruvkaushal1997@gmail.com>
2019-01-26 19:45:31 +0530

    Creating the histogram

--- basic.py
+++ basic.py
@@ -1,4 +1,5 @@
 import nltk
+import re
 from nltk import PorterStemmer
 para = "Thor is played by Chris hemsworth"
 
@@ -44,6 +45,7 @@ named_ent = nltk.ne_chunk(tagged_words)
 named_ent.draw()
 
 #Text modelling uaing bag of words
+#Building the BOW model
 text = """Beans. I was trying to explain to somebody as we were flying in, that's corn. 
 That's beans. And they were very impressed at my agricultural knowledge. Please give it 
 up for Amaury once again for that outstanding introduction. I have a bunch of good friends 
@@ -57,4 +59,8 @@ who set the path for so much outstanding public service here in Illinois.
  Now, I want to start by addressing the elephant in the room. I know people are still 
  wondering why I didn't speak at the commencement."""
  
- 
+dataset = nltk.sent_tokenize(text)
+for i in range(len(dataset)):
+    dataset[i] = dataset[i].lower()
+    dataset[i] = re.sub(r'\W', ' ', dataset[i])
+    dataset[i] = re.sub(r'\s+',' ', dataset[i])
