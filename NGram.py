940a949788e9af0490d8154de58c8798ee8ac30f
DhruvKaushal <dhruvkaushal1997@gmail.com>
2019-01-28 14:43:18 +0530

    completed ngrams by words

--- NGram.py
+++ NGram.py
@@ -1,5 +1,5 @@
 import random
-
+import nltk
 # Sample data
 text = """Global warming or climate change has become a worldwide concern. It is 
 gradually developing into an unprecedented environmental crisis evident in melting
@@ -7,7 +7,7 @@ gradually developing into an unprecedented environmental crisis evident in melti
  droughts. Global warming implies an increase in the average temperature of the 
  Earth due to entrapment of greenhouse gases in the earth’s atmosphere."""
  
-n = 7
+n = 3
 
 #Create ngrams(trigram)
 ngrams = {}
@@ -28,4 +28,26 @@ for i in range(100):
     nextItem = possibility[random.randrange(len(possibility))]
     result += nextItem
     currentGram = result[len(result)-n:len(result)]
+print(result)
+
+#Ngram model by word
+ngrams = {}
+words = nltk.word_tokenize(text)
+for i in range(len(words)-n):
+    gram = ' '.join(words[i:i+n])
+    if gram not in ngrams.keys():
+        ngrams[gram] = []
+    ngrams[gram].append(words[i+n])
+    
+currentGram = ' '.join(words[0:n])
+result = currentGram
+for i in range(30):
+    if currentGram not in ngrams.keys():
+        break
+    possibility = ngrams[currentGram]
+    nextItem = possibility[random.randrange(len(possibility))]
+    result +=' ' + nextItem
+    rwords = nltk.word_tokenize(result)
+    currentGram = ' '.join(rwords[len(rwords)-n:len(rwords)])
+    
 print(result)
